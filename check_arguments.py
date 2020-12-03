e0c0ee3f456d45dc99031dbb403633663e4834d7
radup99 <61940587+radup99@users.noreply.github.com>
2020-12-03 16:55:02 +0200

    added file statistics calculations

--- check_arguments.py
+++ check_arguments.py
@@ -1,4 +1,5 @@
 import sys
+
 default_options = {
     "-l": True,
     "-w": True,
@@ -10,10 +11,11 @@ default_options = {
 long_options = {
     "--lines": "-l",
     "--words": "-w",
-    "--bytes": "-b",
+    "--bytes": "-c",
     "--chars": "-m",
     "--max-line-length": "-L"
 }
+
 
 def check_arguments(args):
     options = {
@@ -25,9 +27,13 @@ def check_arguments(args):
     }
     option_count = 0
 
+    files = []
+    files_count = 0
+
     for arg in args:
         if arg.startswith("--"):
             if arg not in long_options:
+                print(f"wc: unrecognized option \'{arg}\'")
                 return -1, -1
             else:
                 options[long_options[arg]] = True
@@ -35,14 +41,40 @@ def check_arguments(args):
 
         elif arg.startswith("-"):
             if arg not in options:
+                print(f"wc: invalid option -- \'{arg[1:]}\'")
                 return -1, -1
             else:
                 options[arg] = True
                 option_count += 1
 
+        elif arg == "-":
+            files.append("-")
+            files_count += 1
+
+        else:
+            if is_file_valid(arg):
+                files.append(arg)
+                files_count += 1
+            else:
+                return -1, -1
+
     if option_count == 0:
-        return default_options, []
-    return options, []
+        options = default_options
+
+    if files_count == 0:
+        files = ["-"]
+
+    return options, files
+
+
+def is_file_valid(arg):
+    try:
+        f = open(arg)
+    except FileNotFoundError:
+        print(f"wc: {arg}: No such file or directory")
+        return False
+    else:
+        return True
 
 
 def main():
--- wc.py
+++ wc.py
@@ -1,11 +1,60 @@
 from check_arguments import check_arguments
 import sys
+import os
+
+
+def get_file_stats(file, options):
+    f = open(file)
+    text = f.read()
+    stats = []
+
+    if options['-l']:
+        line_count = text.count('\n')
+        stats.append(line_count)
+
+    if options['-w']:
+        word_count = len(text.split())
+        stats.append(word_count)
+
+    if options['-c']:
+        byte_count = os.path.getsize(file)
+        stats.append(byte_count)
+
+    if options['-m']:
+        char_count = len(text)
+        stats.append(char_count)
+
+    if options['-L']:
+        max_len = get_max_line_len(text)
+        stats.append(max_len)
+
+    return stats
+
+
+def get_max_line_len(file):
+    max_len = -1
+
+    for line in file.split('\n'):
+        if len(line) > max_len:
+            max_len = len(line)
+
+    return max_len
+
+
+def process_files(files, options):
+    all_stats = []
+
+    for file in files:
+        stats = get_file_stats(file, options)
+        all_stats.append(stats)
+        print(*stats, file)
 
 
 def main():
     options, files = check_arguments(sys.argv[1:])
-    print(options)
-    print(files)
+    all_stats = []
+
+    process_files(files, options)
 
 
 if __name__ == '__main__':
